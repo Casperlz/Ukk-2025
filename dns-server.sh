283a0645ebf3a798b9c4e0f773dc1e2d5505c4c0
Casperlz <cspmygame@gmail.com>
2025-01-30 11:23:47 +0700

    Create dns-server.sh

---
+++ dns-server.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#install Bins9 sebagai DNS server
+apt install bind9 resolvconf dnsutils -y
+
+#copy file pada bind9
+echo "nama domain kamu"
+read domain
+echo "ip anda"
+read ip
+
+cp -R /etc/bind/db.local /etc/bind/${domain}
+cp -R /etc/bind/db.127 /etc/bind/${domain}.reverse
+
+sed -i "s/localhost/${domain}/g"; "s/::1/${domain}." ${domain}
+sed -i "14s/@/www.${domain}./g" ${domain}
+sed -i "s/127.0.0.1/${ip}/g" ${domain}
+
+echo "Ip belakang anda"
+read ip2
+
+sed -n '13p' ${domain}.reverse >> ${domain}.reverse
+sed -i "s/localhost/${domain}/g" ${domain}.reverse
+sed -i "s/1.0.0/${ip2}/g" ${domain}.reverse
+
+#edit file named.local
+tee /etc/bind/named.conf.local << EOF > /dev/null
+zone "${domain}" {
+        type master;
+        file "/etc/bind/${domain}";
+};
+
+// Reverse Zone
+zone "10.10.10.in-addr.arpa" {
+        type master;
+        file "/etc/bind/${domain}.reverse";
+};
+EOF
+
+#restart named.service
+systemctl restart named.service
+
+#mengganti nameserver
+sed -i "s/127.0.0.53/${ip}/g" /etc/resolv.conf
+sed -i "s/./${domain}/g" /etc/resolv.conf
