0f5d5df6dd1282dacc01e13311098407ac60281b
Casperlz <cspmygame@gmail.com>
2025-01-31 11:06:51 +0700

    Update install-wp.sh

--- install-wp.sh
+++ install-wp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #update dan upgrade
-apt update
+apt update && apt upgrade -y
 
 #install php 8.3
 apt install php8.3-cli php8.3-fpm php8.3-mysql php8.3-opcache php8.3-mbstring php8.3-xml php8.3-gd php8.3-curl -y
