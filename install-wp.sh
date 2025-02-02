2e6fafb81c1055f8fc4eb8a2f920177a3bf9da89
Casperlz <cspmygame@gmail.com>
2025-02-03 06:41:05 +0700

    Update install-wp.sh

--- install-wp.sh
+++ install-wp.sh
@@ -8,12 +8,6 @@ read db_name
 echo "password"
 read pswd
 
-#memulai mysql
-systemctl start mysql
-
-#memulai Nginx
-systemctl start nginx
-
 mysql -u root << EOF
 CREATE DATABASE ${db_name};
 GRANT ALL ON ${db_name}.* TO '${user}'@'localhost' IDENTIFIED BY '${pswd}' WITH GRANT OPTION;
